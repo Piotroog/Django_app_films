18ad44ef1194ef8104469a90ae584b9621a7cb37
Piotr <Piotro.g.98@gmail.com>
2023-12-12 22:22:13 +0100

    corectted templates

--- project_films/urls.py
+++ project_films/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('app_films/', include('app_films.urls')),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view()),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
