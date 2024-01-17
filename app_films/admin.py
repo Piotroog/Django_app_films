8aeb35836fdfad82b0b9fe1a0a261b4259300da2
Piotr <Piotro.g.98@gmail.com>
2024-01-17 21:01:04 +0100

    one to one model

--- app_films/admin.py
+++ app_films/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Film
+from .models import Film, MoreInfo
 
 @admin.register(Film)
 class FilmAdmin(admin.ModelAdmin):
     list_display = ["title", "imdb_rating", "year"]
+
+admin.site.register(MoreInfo)
--- app_films/models.py
+++ app_films/models.py
@@ -1,5 +1,18 @@
 from django.db import models
 
+
+class MoreInfo(models.Model):
+    TYPE = {
+        (0, 'Other'),
+        (1, 'Horror'),
+        (2, 'Sci-fi'),
+        (3, 'Action'),
+        (4, 'Romantic'),
+        (5, 'Comedy'),
+        (6, 'Drama'),
+    }
+    time = models.PositiveSmallIntegerField(default=0)
+    type = models.PositiveSmallIntegerField(default=0, choices=TYPE)
 class Film(models.Model):
     title = models.CharField(max_length=64, blank=False)
     year = models.PositiveSmallIntegerField(default=2000)
@@ -7,6 +20,6 @@ class Film(models.Model):
     premiere = models.DateField(null=True, blank=True)
     imdb_rating = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
     poster = models.ImageField(upload_to="posters", null=True, blank=True)
-
+    bonus = models.OneToOneField(MoreInfo, on_delete=models.CASCADE, null=True, blank=True)
     def __str__(self):
         return self.title + '(' + str(self.year) + ')'
